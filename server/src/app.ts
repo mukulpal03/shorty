311c2a49d75fe0324962c51ef6eee082b738919c
Mukul Pal <mukulpal0333@gmail.com>
2026-05-18 17:35:37 +0530

    feat: add basic express boilerplate

---
+++ server/src/app.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(express.json());
+app.use(
+  cors({
+    origin: process.env.ORIGIN_URL,
+    credentials: true,
+  }),
+);
+
+export default app;
