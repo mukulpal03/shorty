e3cf010870b58d7c27a97c85e8cd5c14afda6cfe
Mukul Pal <mukulpal0333@gmail.com>
2026-05-19 16:21:03 +0530

    feat: add db model for storing urls

---
+++ server/src/modules/url/url.model.ts
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const urlSchema = new mongoose.Schema(
+  {
+    originalUrl: {
+      type: String,
+      required: true,
+    },
+    shortUrl: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+  },
+  { timestamps: true },
+);
+
+const Url = mongoose.model("Url", urlSchema);
+
+export default Url;
