9d6ae8e143a644250346a7e1d4e9d8e346358363
Mukul Pal <mukulpal0333@gmail.com>
2026-05-19 16:18:19 +0530

    feat: setup db connection

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,8 @@
 import app from "./app";
+import { connectDB } from "./config/db";
 
 const PORT = process.env.PORT ?? 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-});
+connectDB().then(() =>
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`)),
+);
