d28f467c6b80480a256965a40371f258ce62f2e2
Mukul Pal <mukulpal0333@gmail.com>
2026-06-16 16:30:13 +0530

    fix: remove the shortcode validation

--- server/src/modules/url/url.constants.ts
+++
@@ -1,3 +0,0 @@
-export const SHORT_CODE_LENGTH = 7;
-
-export const SHORT_CODE_PATTERN = /^[A-Za-z0-9_-]{7}$/;
--- server/src/modules/url/url.redirect.routes.ts
+++ server/src/modules/url/url.redirect.routes.ts
@@ -1,25 +1,9 @@
-import { Router, type NextFunction, type Request, type Response } from "express";
-
+import { Router } from "express";
 import { asyncHandler } from "../../middleware/asyncHandler";
-import { SHORT_CODE_PATTERN } from "./url.constants";
 import { retrieveOriginalUrlController } from "./url.controller";
 
 const router = Router();
 
-function requireValidShortCode(req: Request, _res: Response, next: NextFunction) {
-  const { shortUrl } = req.params;
-
-  if (!shortUrl || !SHORT_CODE_PATTERN.test(shortUrl as string)) {
-    return next();
-  }
-
-  return next();
-}
-
-router.get(
-  "/:shortUrl",
-  requireValidShortCode,
-  asyncHandler(retrieveOriginalUrlController),
-);
+router.get("/:shortUrl", asyncHandler(retrieveOriginalUrlController));
 
 export default router;
